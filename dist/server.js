cfd3307c32cbfc320cf062d53125b6421a53ef76
mit <meetrakholiya31@gmail.com>
2026-01-30 13:54:20 +0530

    update

--- dist/server.js
+++
@@ -1,9 +0,0 @@
-import app from "./app.js";
-import connectDB from "./config/db.js";
-import dotenv from "dotenv";
-dotenv.config();
-connectDB();
-const PORT = process.env.PORT || 6000;
-app.listen(PORT, () => {
-    console.log(`You Server Is Started in ${PORT}`);
-});
